56b450f1958aaf1200b99dd99f008e872188ac43
Alessio Limina <alessio.limina@gmail.com>
2023-01-24 16:25:07 +0100

    Exercise: Other operators

--- src/it/develhope/otheroperators/Start.java
+++ src/it/develhope/otheroperators/Start.java
@@ -3,36 +3,33 @@ package it.develhope.otheroperators;
 public class Start {
     public static void main(String[] args) {
 
+        System.out.println("------Starting operations----------");
+
         int a = 15;
 
-        System.out.println(a);
+        System.out.println("The A value is " + a);
+        System.out.println("The new A value is " + (a += 5));
+        System.out.println("The new A value is " + (a -= 4));
 
-        System.out.println(a += 5);;
-
-        System.out.println(a -= 4);;
+        System.out.println("-------------------------------------");
 
         int b = a;
 
-        System.out.println(b);
-
-        System.out.println(++b);
+        System.out.println("The B value is " + b);
+        System.out.println("The new B value is " + ++b);
 
         if (b % 2 != 0) {
-
-            System.out.println("b is an odd number");
-
+            System.out.println("B is an odd number");
         } else {
-
-            System.out.println("b is an even number");
+            System.out.println("B is an even number");
         }
+
         if (b * (b + 1) % 3 == 0) {
-
             System.out.println("The number is a multiple of 3");
-
         } else {
             System.out.println("The number isn't a multiple of 3");
-
         }
+        System.out.println("----------------------------------------");
     }
 }
 
